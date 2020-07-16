8a9d269a49374e8c190a02d36d5d5b3a1761ac0b
nicholas DOOGE <ndooge@c3r7s4.wethinkcode.co.za>
2020-07-16 16:14:01 +0200

    1,2

---
+++ ex00/ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndooge <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/16 15:04:52 by ndooge            #+#    #+#             */
+/*   Updated: 2020/07/16 15:29:01 by ndooge           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int	len;
+	int i;
+
+
+	len = 0;
+	i = 0;
+	while (src[len])
+		len++;
+	dest = (char *)malloc(sizeof(*dest) * len + 1);
+	while (i < len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return dest;
+}
+
---
+++ ex01/ft_range.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndooge <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/16 15:38:01 by ndooge            #+#    #+#             */
+/*   Updated: 2020/07/16 15:39:02 by ndooge           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		*ft_range(int min, int max)
+{
+	int *tab;
+	int i;
+
+	if (min >= max)
+		return NULL;
+	tab = (int*)malloc(sizeof(*tab) * (max - min));
+	i = 0;
+	while (min < max)
+	{
+		tab[i] = min;
+		i++;
+		min++;
+	}
+	return tab;
+}
